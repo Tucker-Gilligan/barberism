728376049507bc96f1e422e6c1703050dea4b489
Tucker Gilligan <tucker@Tuckers-MacBook-Pro.local>
2021-01-16 13:23:52 -0500

    commit to refactor branch

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Barbers from "./Barber/Barbers";
-import Customer from "./Client/Customer";
+
 
 export default function Home() {
   return (
