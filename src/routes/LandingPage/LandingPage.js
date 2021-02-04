7f99b8b03f3fb0e8e2713879683b79b60f0dc8ef
Tucker <gillitux@gmail.com>
2021-02-04 17:43:07 -0500

    updated buttons on landing page to be 'hamburger' style (stacked) and larger, hover on buttons produces a pointer

--- src/routes/LandingPage/LandingPage.js
+++ src/routes/LandingPage/LandingPage.js
@@ -7,13 +7,15 @@ export default function LandingPage() {
         Welcome to <span>Barberism</span>
       </h2>
       <h3>Connecting you to barbers to meet your individualized needs.</h3>
-      <h4>Get Started</h4>
-      <div className="get__started__links">
-        <div>
-          <Link to="/locate-barber">Locate a Barber</Link>
-        </div>
-        <div>
-          <Link to="/register-barber">Register a Barber</Link>
+      <div className="get__started">
+        <h4>Get Started</h4>
+        <div className="get__started__links">
+          <div className="get__started__link">
+            <Link to="/locate-barber">Locate a Barber</Link>
+          </div>
+          <div className="get__started__link">
+            <Link to="/register-barber">Register a Barber</Link>
+          </div>
         </div>
       </div>
     </div>
